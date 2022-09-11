01d5b479f99a22a5a738809dd940538c19c31182
pixuszin <pixupixu41@gmail.com>
2022-09-11 13:08:23 -0300

    initial commit

---
+++ sqliscan.py
@@ -0,0 +1,44 @@
+import copy
+import sys
+from urllib import parse
+
+import requests
+
+
+def request(url):
+    headers = {"User-Agent":
+                   "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"}
+    try:
+        response = requests.get(url, headers=headers)
+        html = response.text
+        return html
+    except:
+        pass
+
+
+def is_vulnerable(html):
+    errors = ["mysql_fetch_array()",
+              "Voce tem um erro na SQL Sintaxe"]
+    for error in errors:
+        if error in html:
+            return True
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    url_parsed = parse.urlsplit(url)
+    params = parse.parse_qs(url_parsed.query)
+    for param in params.keys():
+        query = copy.deepcopy(params)
+        for c in "'\"":
+            query[param][0] = c
+            new_params = parse.urlencode(query, doseq=True)
+            url_final = url_parsed._replace(query=new_params)
+            url_final = url_final.geturl()
+            html = request(url_final)
+            if html:
+                if is_vulnerable(html):
+                    print("[ + ] {} parametro é vulneravel".format(param))
+                    quit()
+
+    print("Não vulneravel")
